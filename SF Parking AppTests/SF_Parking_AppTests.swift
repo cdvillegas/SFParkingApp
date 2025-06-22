3ba21955c4a9947f8dcbf1cc9a99fccd2786f388
cdvillegas <cdvillegas11@gmail.com>
2025-06-22 15:41:20 -0700

    Initial Commit

---
+++ SF Parking AppTests/SF_Parking_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SF_Parking_AppTests.swift
+//  SF Parking AppTests
+//
+//  Created by Chris Villegas on 6/22/25.
+//
+
+import Testing
+@testable import SF_Parking_App
+
+struct SF_Parking_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
